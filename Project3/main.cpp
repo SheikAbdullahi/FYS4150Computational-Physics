dc2f2e002fdd6eeea85979057e215c843207b181
Abdullahi Sheik <abdullah@oslomet.no>
2023-10-23 22:37:59 +0200

    new changes on main function

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,43 +1,22 @@
-// main.cpp
-#include <iostream>
-#include <vector>
-#include "Particle.h"
 #include "PenningTrap.h"
+#include "Particle.h"
+#include <iostream>
 
 int main() {
-    // Constants for the PenningTrap
-    double B0 = 9.65e1;  // magnetic field strength
-    double V0 = 9.65e6;  // electric potential
-    double d = 500;      // characteristic dimension
-    double ke = 1.38935333e5;  // Coulomb constant
+    // Create a Penning trap instance
+    PenningTrap myTrap(1e4, 1e8, 1e2);  // Example values for B0, V0, and d
 
-    // Create a few particles
-    Particle p1(1, 1, {0, 0, 0}, {0, 0, 0});  // A particle with charge 1e, mass 1u, position (0, 0, 0), and velocity (0, 0, 0)
-    Particle p2(1, 1, {10, 0, 0}, {0, 1, 0});  // Another particle, different position and velocity
+    // Create some particles
+    Particle p1(1.602e-19, 9.109e-31, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0});
+    Particle p2(-1.602e-19, 9.109e-31, {0.1, 0.0, 0.0}, {0.0, 0.0, 0.0});
 
-    // Store them in a vector
-    std::vector<Particle> particles = {p1, p2};
+    // Add particles to the trap
+    myTrap.add_particle(p1);
+    myTrap.add_particle(p2);
 
-    // Create a PenningTrap with those particles
-    PenningTrap trap(B0, V0, d, ke, particles);
-
-    // Time evolution parameters
-    double dt = 0.1;  // time step
-    int steps = 1000;  // number of steps to simulate
-
-    // Simulate some time evolution
-    for (int i = 0; i < steps; ++i) {
-        trap.updateParticles(dt);
-
-        // Output the state at each step, or every few steps, as needed
-        std::cout << "After step " << i+1 << ":" << std::endl;
-        for (size_t j = 0; j < particles.size(); ++j) {
-            std::cout << "  Particle " << j+1 << " position: " 
-                      << particles[j].position[0] << ", "
-                      << particles[j].position[1] << ", "
-                      << particles[j].position[2] << std::endl;
-        }
-    }
+    // Calculate and display forces, for example, the total force on the first particle
+    arma::vec force = myTrap.total_force(0);
+    force.print("Total force on particle 1:");
 
     return 0;
 }
